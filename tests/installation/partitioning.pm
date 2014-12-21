dbaa942cc4d4ac25ae451dc21d681f5a61ea8ace
openQA user <rbrownccb@opensuse.org>
2014-12-21 16:56:39 +0000

    Minor improvements to partitioning and welcome modules

--- tests/installation/partitioning.pm
+++ tests/installation/partitioning.pm
@@ -4,11 +4,11 @@ use testapi;
 
 sub run() {
 
-    assert_screen "inst-disk-not-conf", 5;
+    assert_screen "inst-disk-not-conf", 10;
     send_key 'alt-d';
-    assert_screen "inst-part-selected", 5;
+    assert_screen "inst-part-selected", 10;
     send_key 'ret';
-    assert_screen "inst-destination-status", 5;
+    assert_screen "inst-destination-status", 10;
     send_key 'alt-d';
     assert_screen "inst-auto-partitioning", 60;
  
--- tests/installation/welcome.pm
+++ tests/installation/welcome.pm
@@ -7,8 +7,16 @@ sub run() {
     assert_screen "inst-welcome", 120;
     send_key 'right';
     send_key 'ret';
-    assert_screen "inst-langselection", 30;
+    assert_screen "inst-langselection", 60;
     send_key 'alt-c';
+    
+    if (get_var("BETA")) {
+        assert_screen "inst-betawarning", 30;
+        send_key "tab";
+        assert_screen "inst-fateaccept", 5;
+        send_key "ret";    
+    }
+    
     assert_screen "inst-summary", 5;
  
 }
